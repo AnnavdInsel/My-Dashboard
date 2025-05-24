88a5942d36f5b261612803766085abfeac7d9aa0
FlowsenFresh <bullseye92@freenet.de>
2025-05-24 15:53:06 +0200

    todo erweitert 24.05 15:53 Uhr (update#1)

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -2,13 +2,44 @@
   <div>
     <h2>📝 ToDo</h2>
     <p>Hier kommt deine ToDo-Liste rein.</p>
+
+    <input type="text" v-model="task" placeholder="Gib hier deine Aufgabe ein" />
+    <br />
+    <h3>Heutigen Aufgaben:</h3>
+    <p>{{ taskOutput }}</p>
+    <button @click="saveTask">Speichern</button>
   </div>
 </template>
 
 <script setup>
 // Platz für Logik
+import { ref } from 'vue'
+
+const task = ref('')
+const taskOutput = ref('Hier stehen die zu erledigenden Aufgaben')
+
+function saveTask() {
+  taskOutput.value = task.value
+}
 </script>
 
 <style scoped>
 /* Optionales Styling */
+#taskInput {
+  margin: 0.5em;
+  font-size: 20px;
+  padding: 10px;
+  text-align: center;
+}
+
+h3 {
+  text-decoration: underline;
+  font-size: 25px;
+}
+
+#taskOutput {
+  font-size: 18px;
+  background-color: grey;
+  height: 200px;
+}
 </style>
