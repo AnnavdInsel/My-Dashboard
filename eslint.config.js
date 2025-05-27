65b812364243c638dae6e78aff1684f54b3b5aaf
FlowsenFresh <bullseye92@freenet.de>
2025-05-27 23:25:25 +0200

    eslintconfig verbessert durch gpt

--- eslint.config.js
+++ eslint.config.js
@@ -25,7 +25,6 @@ export default defineConfig([
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
 
-  // ⬇️ NEU: Prettier als Regel aktivieren
   {
     plugins: {
       prettier: pluginPrettier,
